b07348b4f48958873f5a090f9a55b88e6c5e1329
VogtD01 <vd6806@mci4me.at>
2024-04-15 16:26:07 +0200

    final push 15.4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from read_data import load_person_data, get_person_list
+from read_data import load_person_data, get_person_list, find_person_data_by_name
+from PIL import Image
 
 st.write("# EKG APP")
 st.write("## Versuchsperson auswählen")
@@ -8,16 +9,20 @@ st.write("## Versuchsperson auswählen")
 if 'current_user' not in st.session_state:
     st.session_state.current_user = 'None'
 
-
-st.write("Der Name ist: ", st.session_state.current_user)
-
 # Funktionen aufrufen
 person_names = get_person_list()
-
 
 # Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
     options = person_names, key="sbVersuchsperson")
+st.write("Der Name ist: ", st.session_state.current_user)
+
+# Laden eines Bilds
+person = find_person_data_by_name(st.session_state.current_user)
+image = Image.open(person["picture_path"])
+# Anzeigen eines Bilds mit Caption
+st.image(image, caption=st.session_state.current_user)
+
 
 #streamlit run main.py
--- read_data.py
+++ read_data.py
@@ -9,7 +9,24 @@ def load_person_data():
 
 def get_person_list():
     person_list = []
-    for person in load_person_data():
+    person_data = load_person_data()
+    for person in person_data:
         person_list.append(person["firstname"] + " " + person["lastname"] )
       
     return person_list
+
+def find_person_data_by_name(name):
+    person_data = load_person_data()
+    for person in person_data:
+        if name == person["firstname"] + " " + person["lastname"]:
+            return person
+    return None
+    
+        
+if __name__ == "__main__":
+    print(load_person_data())
+    print(get_person_list())
+    print(find_person_data_by_name("Max Mustermann"))
+
+
+
