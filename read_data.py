49901c967be8ff8e0e132f017478812c4bf6bb5d
VogtD01 <vd6806@mci4me.at>
2024-04-15 14:19:32 +0200

    read data update

--- read_data.py
+++ read_data.py
@@ -1,9 +1,15 @@
 import json
 
-# Opening JSON file
-file = open("person_db.json")
 
-# Loading the JSON File in a dictionary
-person_data = json.load(file)
+def load_person_data():
+    """A Function that knows where te person Database is and returns a Dictionary with the Persons"""
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    return person_data
 
-person_data
+def get_person_list():
+    person_list = []
+    for person in load_person_data():
+        person_list.append(person["firstname"] + " " + person["lastname"] )
+      
+    return person_list
